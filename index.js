b5f448af8aa8f46305145776eb4bda12516463d8
Kai Thomas <45699223+kaiThomas236@users.noreply.github.com>
2022-11-15 15:42:10 -0700

    browser now remembers chosen theme

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-var head = document.firstElementChild.firstElementChild;
-var body = document.firstElementChild.lastElementChild;
+const colorThemes = document.querySelectorAll('[name="theme"]');
+
+const storeTheme = function (theme) {
+    localStorage.setItem("theme", theme);
+}
+
+const getTheme = function () {
+    const activeTheme = localStorage.getItem("theme");
+    console.log(activeTheme);
+    colorThemes.forEach((themeOption) => {
+        if (themeOption.id === activeTheme) {
+            themeOption.checked = true;
+        }
+    });
+};
+
+colorThemes.forEach(theme => {
+    theme.addEventListener('click', () => {
+        storeTheme(theme.id);
+    });
+});
+
+document.onload = getTheme();
