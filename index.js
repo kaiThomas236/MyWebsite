e3a3cfea16b5e32ecfe2c39cb28efe0bb8b9cafb
Kai Thomas <45699223+kaiThomas236@users.noreply.github.com>
2022-11-15 21:40:38 -0700

    working on firefox now

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const colorThemes = document.querySelectorAll('[name="theme"]');
+const isFirefox = window.navigator.userAgent.indexOf("Firefox") > -1;
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || !!navigator.userAgent.match(/Trident.*rv\:11\./);
+
 
 const storeTheme = function (theme) {
     localStorage.setItem("theme", theme);
@@ -6,16 +9,24 @@ const storeTheme = function (theme) {
 
 const getTheme = function () {
     const activeTheme = localStorage.getItem("theme");
-    console.log(activeTheme);
+    
     colorThemes.forEach((themeOption) => {
         if (themeOption.id === activeTheme) {
             themeOption.checked = true;
         }
     });
+    document.documentElement.className = activeTheme;
 };
+
+const backup = function (theme) {
+    document.documentElement.className = theme;
+}
 
 colorThemes.forEach(theme => {
     theme.addEventListener('click', () => {
+        if (isFirefox || isIE) {
+            backup(theme.id);
+        }
         storeTheme(theme.id);
     });
 });
